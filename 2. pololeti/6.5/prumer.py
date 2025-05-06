c495e9ea44327badacbeff630cc2227656a1a40f
IamSimon196 <143520535+IamSimon196@users.noreply.github.com>
2025-05-06 09:19:06 +0200

    Add files via upload

--- 2. pololeti/6.5/prumer.py
+++ 2. pololeti/6.5/prumer.py
@@ -1,12 +1,12 @@
 nb = int(input("Zadej prvni cislo intervalu:\n"))
 ne = int(input("Zadej druhe cislo intervalu:\n"))
 
-def prumer(nb: int, ne: int) -> float:
-    ans = 0
+def prumer(nb: int, ne: int) -> tuple:
+    ans1 = 0
 
     for i in range(nb, ne+1):
-        ans += i
-    ans = ans/(ne+1-nb)
-    return ans
+        ans1 += i
+    ans2 = ans1/(ne+1-nb)
+    return ans1, ans2
 
-print(f"Prumer cisel v intervalu od {nb} do {ne} je {prumer(nb, ne)}")
+print(f"Soucet a prumer cisel v intervalu od {nb} do {ne} je {prumer(nb, ne)}")
