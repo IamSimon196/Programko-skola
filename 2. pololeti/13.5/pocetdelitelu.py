02aca79fae34be16d228848c2a68e0b8c7fc5163
IamSimon196 <143520535+IamSimon196@users.noreply.github.com>
2025-05-13 09:06:18 +0200

    Add files via upload

---
+++ 2. pololeti/13.5/pocetdelitelu.py
@@ -0,0 +1,10 @@
+n = int(input("Zadej cele cislo:\n"))
+
+def delitele(n: int):
+    ans = []
+    for i in range(1,n):
+        if n % i == 0:
+            ans.append(i)
+    return ans
+
+print(f"Cislo n {n} ma: {len(delitele(n))} delitelu")
