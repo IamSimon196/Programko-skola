4516d36b825a97f6038626d82e64a4ba7a2fd3c3
IamSimon196 <143520535+IamSimon196@users.noreply.github.com>
2025-05-13 08:44:42 +0200

    Add files via upload

--- 2. pololeti/13.5/dokonalyinterval.py
+++ 2. pololeti/13.5/dokonalyinterval.py
@@ -1,14 +1,16 @@
 begin = int(input("Zadej pocatecni cislo:\n"))
 end = int(input("Zadej konecne cislo:\n"))
 
-def dokonale(n: int):
-    ans = 0
-    for i in range(1,n):
-        if n % i == 0:
-            ans += i
-    return ans == n
+def dokonale(beg: int, end: int):
+    ans_list = []
+    for n in range(beg, end):
+        ans = 0  
+        for i in range(1, n):
+            if n % i == 0:
+                ans += i
+        if ans == n:
+            ans_list.append(n) 
+    return ans_list
 
-for n in range(begin, end):
-    print(f"Cislo {n} je dokonale cislo: {dokonale(n)}")
+print(f"V intervalu {begin}, {end} je dokonale cislo: {dokonale(begin, end)}")
 
-#6, 28, 496
