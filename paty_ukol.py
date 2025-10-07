ad6b82bf1d3b50cdb11ec32ccba2de277300fe5b
IamSimon196 <143520535+IamSimon196@users.noreply.github.com>
2025-10-07 09:09:52 +0200

    Add files via upload

---
+++ paty_ukol.py
@@ -0,0 +1,9 @@
+with open("/run/media/simon/FLESKA/souborz python/Cisla.txt", "r", encoding="utf-8") as file:
+    obsah = file.read()
+    list_cisel = []
+    for line in obsah.split("\n"):
+        list_cisel.append(int(line))
+
+    print("List cisel je:", list_cisel)
+    print(f"Mean cisel je {sum(list_cisel)/len(list_cisel)}")
+    print(f"Median cisel je {sorted(list_cisel)[len(list_cisel)//2]}") 
